6df2888e547de21968e10db9250deb54896100ea
bek031291 <bek031291@yandex.com>
2025-03-04 21:49:21 +0500

    added new func

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
-func main() {
+import "log"
 
+func main() {
+	sum := calculator(1, "+", 5)
+	min := calculator(1, "-", 5)
+	zarb := calculator(1, "*", 5)
+	taqsim := calculator(100, "/", 5)
+	log.Println("sum=====> ", sum)
+	log.Println("min=====> ", min)
+	log.Println("zarb====> ", zarb)
+	log.Println("taqsim==> ", taqsim)
 }
 
-func test() {
-
+func calculator(first int, amal string, second int) int {
+	switch amal {
+	case "+":
+		return first + second
+	case "-":
+		return first - second
+	case "*":
+		return first * second
+	case "/":
+		return first / second
+	default:
+		return first + second
+	}
 }
